548834a85ee2d611c07a9a8d82a5734b863a33fa
Janne Mattila <janne.mattila@hotmail.fi>
2021-05-31 15:35:28 +0300

    Add Logic App example workflow

--- deploy.ps1
+++ deploy.ps1
@@ -3,7 +3,7 @@ $arcName = "aks-arc"
 $customLocationName = "jannewest"
 $resourceGroup = "rg-k8s-appsvc-demo"
 $location = "westeurope"
-$namespace="appservice-ns"
+$namespace = "appservice-ns"
 $extensionInstanceName = "appsvcextension"
 $namespace = "appservice-ns"
 $kubeAppServiceEnvironment = "kube-ase"
@@ -44,8 +44,8 @@ az group create --name $resourceGroup --location $location -o table
 # https://docs.microsoft.com/en-us/azure/app-service/overview-arc-integration#public-preview-limitations
 # Create AKS 
 az aks create --resource-group $resourceGroup --name $aksName --enable-aad --node-count 1 --enable-cluster-autoscaler --min-count 1 --max-count 3 --node-vm-size Standard_B2ms --max-pods 150
-$infraResourceGroup=(az aks show --resource-group $resourceGroup --name $aksName --query nodeResourceGroup --output tsv)
-$staticIp=(az network public-ip create --resource-group $infraResourceGroup --name MyPublicIP --sku STANDARD --query publicIp.ipAddress --output tsv)
+$infraResourceGroup = (az aks show --resource-group $resourceGroup --name $aksName --query nodeResourceGroup --output tsv)
+$staticIp = (az network public-ip create --resource-group $infraResourceGroup --name MyPublicIP --sku STANDARD --query publicIp.ipAddress --output tsv)
 $staticIp
 az aks get-credentials --resource-group $resourceGroup --name $aksName --admin --overwrite-existing
 
@@ -60,13 +60,13 @@ az connectedk8s connect --name $arcName --resource-group $resourceGroup
 az connectedk8s list --resource-group $resourceGroup -o table
 
 # View Azure Arc agents in cluster
-kubectl get deployments,pods -n azure-arc
+kubectl get deployments, pods -n azure-arc
 
 # Enable custom locations on cluster
 az connectedk8s enable-features --name $arcName --resource-group $resourceGroup --features cluster-connect custom-locations
 
 # Get Arc enabled Kubernetes cluster identifier
-$connectedClusterId=(az connectedk8s show --name $arcName --resource-group $resourceGroup --query id -o tsv)
+$connectedClusterId = (az connectedk8s show --name $arcName --resource-group $resourceGroup --query id -o tsv)
 $connectedClusterId
 
 # Enable Azure App Service on Azure Arc extension
@@ -83,9 +83,9 @@ $extensionId = az k8s-extension create -g $resourceGroup --name $extensionInstan
     --configuration-settings "buildService.storageAccessMode=ReadWriteOnce" `
     --configuration-settings "customConfigMap=$namespace/kube-environment-config" `
     --configuration-settings "envoy.annotations.service.beta.kubernetes.io/azure-load-balancer-resource-group=$resourceGroup" # `
-    # --configuration-settings "logProcessor.appLogs.destination=log-analytics" `
-    # --configuration-protected-settings "logProcessor.appLogs.logAnalyticsConfig.customerId=$logAnalyticsWorkspaceIdEnc" `
-    # --configuration-protected-settings "logProcessor.appLogs.logAnalyticsConfig.sharedKey=$logAnalyticsKeyEnc"
+# --configuration-settings "logProcessor.appLogs.destination=log-analytics" `
+# --configuration-protected-settings "logProcessor.appLogs.logAnalyticsConfig.customerId=$logAnalyticsWorkspaceIdEnc" `
+# --configuration-protected-settings "logProcessor.appLogs.logAnalyticsConfig.sharedKey=$logAnalyticsKeyEnc"
 
 # Verify install state
 # az k8s-extension show --name $extensionInstanceName --cluster-type connectedClusters -c $arcName --resource-group $resourceGroup --query installState -o tsv
@@ -99,7 +99,7 @@ kubectl get svc -A
 
 # Create custom location
 az customlocation create -n $customLocationName --resource-group $resourceGroup --namespace $namespace --host-resource-id $connectedClusterId --cluster-extension-ids $extensionId
-$customLocationId=(az customlocation show -n $customLocationName --resource-group $resourceGroup --query id -o tsv)
+$customLocationId = (az customlocation show -n $customLocationName --resource-group $resourceGroup --query id -o tsv)
 $customLocationId
 
 # Create App Service Environment
@@ -114,17 +114,41 @@ az appservice kube show `
     --resource-group $resourceGroup `
     --name $kubeAppServiceEnvironment
 
+####################
 # Create App Service
+####################
 $image = "jannemattila/echo"
 $appServiceName = "echofromkube"
 $appServicePlanName = "asp"
 
 az appservice plan create --name $appServicePlanName --resource-group $resourceGroup --custom-location $customLocationId --is-linux --per-site-scaling --sku K1
-az webapp create --name $appServiceName --plan $appServicePlanName --custom-location $customLocationId --resource-group $resourceGroup -i $image -o table
+$webAppUri = (az webapp create --name $appServiceName --plan $appServicePlanName --custom-location $customLocationId --resource-group $resourceGroup -i $image --query defaultHostName -o TSV)
+$webAppUri
 
+$url = "http://$webAppUri/api/echo"
+$data = @{
+    firstName = "John"
+    lastName  = "Doe"
+}
+$body = ConvertTo-Json $data
+Invoke-RestMethod -Body $body -ContentType "application/json" -Method "POST" -DisableKeepAlive -Uri $url
+
+###################
 # Create Logic App
+###################
+az extension remove --name logicapp
 az extension add --yes --source "https://aka.ms/logicapp-latest-py2.py3-none-any.whl"
-az logicapp create --resource-group $resourceGroup --name MyLogicAppName --custom-location $customLocationId
+az logicapp create --resource-group $resourceGroup --name wffromkube --custom-location $customLocationId --storage-account yourstorage
+
+# Use VS Code for deploying the app
+
+# Update url
+$logicAppUrl = "https://wffromkube.kube-ase-abcdefg.westeurope.k4apps.io:443/api/TemperatureConverter/triggers/manual/invoke"
+$logicAppData = @{
+    temperature  = 33
+}
+$logicAppBody = ConvertTo-Json $logicAppData
+Invoke-RestMethod -Body $logicAppBody -ContentType "application/json" -Method "POST" -DisableKeepAlive -Uri $logicAppUrl
 
 # Wipe out the resources
 az group delete --name $resourceGroup -y
